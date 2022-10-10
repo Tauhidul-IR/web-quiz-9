43360621a6b5be10de8eb059a796ca50770e7cef
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-11 05:50:33 +0600

    catagory

--- src/component/Catagory/Catagory.js
+++ src/component/Catagory/Catagory.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Catagory = ({ catagory }) => {
     const { logo, name, id } = catagory;
     return (
-        <div>
+        <div className='bg-white p-5 text-black mb-8'>
             <div>
-                <img className='w-64' src={logo} alt="" ></img>
-                <h3>{name}</h3>
+                <img className='w-64 bg-slate-400 mr-0 mb-2' src={logo} alt="" ></img>
+                <div className='flex w-full justify-between'>
+                    <h3 className='text-2xl'>{name}</h3>
+                    <button className='bg-blue-600 px-3 py-2 rounded-lg text-lg cursor-pointer'>Start Practice <span><FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></span></button>
+
+                </div>
             </div>
         </div>
     );
