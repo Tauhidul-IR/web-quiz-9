d505e8eeddd05a18daad487d327c4a597cca2e1b
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-11 20:42:58 +0600

    chart-responsive

--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -8,16 +8,32 @@ const Statistics = () => {
 
     return (
         <div className='bg-white'>
-            <h1>This is Statistics page</h1>
+            <h1 className='text-black text-3xl py-6'>This is Statistics page</h1>
             <div>
-                <LineChart width={500} height={500} data={data.data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <Line connectNulls type="monotone" dataKey="total" stroke="#8884d8" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip></Tooltip>
 
-                </LineChart>
+                <div style={{ width: '100%' }}>
+                    <ResponsiveContainer width="100%" height={200}>
+                        <LineChart
+                            width={500}
+                            height={200}
+                            data={data.data}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Line type="monotone" dataKey="total" stroke="#8884d8" fill="#8884d8" />
+                        </LineChart>
+                    </ResponsiveContainer>
+
+
+                </div>
             </div>
         </div>
     );
