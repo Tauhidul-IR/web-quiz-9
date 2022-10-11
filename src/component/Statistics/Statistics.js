088d339e53463ba7a7ce4b92717f09a70d96c4d8
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-11 18:37:49 +0600

    chart

--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+    // console.log(data.data)
+
     return (
         <div>
             <h1>This is Statistics page</h1>
+            <div>
+                <LineChart width={500} height={500} data={data.data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Line connectNulls type="monotone" dataKey="total" stroke="#8884d8" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+
+                </LineChart>
+            </div>
         </div>
     );
 };
