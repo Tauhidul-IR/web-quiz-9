73126417ec7ea08a2ea37f163e17a3e107059d93
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-11 07:39:03 +0600

    eyeBtn

--- src/component/Questions/Questions.js
+++ src/component/Questions/Questions.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Options from '../Options/Options';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = ({ qst }) => {
-    // console.log(qst)
     const { correctAnswer, options, question, id } = qst;
+
+    const handleEyeCorrect = () => {
+        toast.success(correctAnswer);
+    }
+    // console.log(qst)
+
     return (
         <div className='w-1/2 mx-auto border bg-cyan-100 rounded mt-3'>
             <div>
-                <h1 className='text-2xl'>Question : {question}</h1>
+                <h1 className='text-2xl mb-3'>Question : {question}</h1>
+                <button onClick={() => handleEyeCorrect()}><ToastContainer /><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></button>
             </div>
             <div>
                 {
