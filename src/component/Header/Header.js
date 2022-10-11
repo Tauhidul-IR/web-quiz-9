3cfc295bc1f0980d39d005106a4ab2f3be1036ab
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-11 19:44:58 +0600

    header-resposive

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
         <div className='py-5 bg-slate-500'>
-            <div onClick={() => setOpen(!open)} className="h-7 w-7 md:hidden">
-                {
-                    open ? <XMarkIcon /> : <Bars3Icon />
-                }
-            </div>
-            <div className='flex justify-between container mx-auto'>
+
+            <div className='flex sm:flex-row flex-col  justify-between container mx-auto'>
                 <div>
                     <h1 className='text-4xl text-white'>Web-Quiz</h1>
                 </div>
-                <div>
-                    <div className={`text-white md:flex justify-center w-full absolute md:static duration-500 ${open ? 'top-6' : "top-[-120px]"}`}>
-                        <NavLink className={'mr-3'} NavLink to={'/'} > Home</NavLink >
-                        <NavLink className={'mr-3'} to={'/about'}>About</NavLink>
-                        <NavLink className={'mr-3'} to={'/statistics'}>Statistics</NavLink>
-                        <NavLink className={'mr-3'} to={'/blogs'}>Blogs</NavLink>
-                    </div >
+                <div className='text-black text-xl flex sm:flex-row flex-col'>
+                    <NavLink className={'mr-3'} NavLink to={'/'} > Home</NavLink >
+                    <NavLink className={'mr-3'} to={'/about'}>About</NavLink>
+                    <NavLink className={'mr-3'} to={'/statistics'}>Statistics</NavLink>
+                    <NavLink className={'mr-3'} to={'/blogs'}>Blogs</NavLink>
+
                 </div>
             </div>
         </div >
